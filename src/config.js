d56d7641f44be664cd53f881181a2c4eeadb055c
josno <jnsenoren@gmail.com>
2020-04-01 16:23:36 +0400

    updated age

--- src/config.js
+++ src/config.js
@@ -1,12 +1,11 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
-	NODE_ENV: process.env.NODE_ENV || 'development',
+	NODE_ENV: process.env.NODE_ENV || "development",
 	DATABASE_URL:
-		process.env.DATABASE_URL ||
-		'postgres://pawpad_user@localhost:5432/pawpad',
-	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-	CLIENT_ORIGIN: 'https://dogge-pawpad.now.sh',
-	// CLIENT_ORIGIN: 'http://localhost:3000',
+		process.env.DATABASE_URL || "postgres://pawpad_user@localhost:5432/pawpad",
+	JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+	// CLIENT_ORIGIN: 'https://dogge-pawpad.now.sh',
+	CLIENT_ORIGIN: "http://localhost:3000",
 	CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
 	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
 	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
