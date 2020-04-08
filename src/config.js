92680fd30c010bcbad8d9dbcfdab68649247f644
josno <jnsenoren@gmail.com>
2020-04-08 17:33:17 +0400

    changed client origin

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ module.exports = {
 	DATABASE_URL:
 		process.env.DATABASE_URL || "postgres://pawpad_user@localhost:5432/pawpad",
 	JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
-	CLIENT_ORIGIN: "https://dogge-pawpad.now.sh",
+	CLIENT_ORIGIN: "https://dogge.usepawpad.com",
 	// CLIENT_ORIGIN: "http://localhost:3000",
 	CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
 	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
