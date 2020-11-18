546bb0bec7c4c7f44f36fd92460c0495e4f9c394
josno <jnsenoren@gmail.com>
2020-11-18 21:37:59 +0400

    Updated env

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	PORT: 8000,
+	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || "development",
 	DATABASE_URL:
 		process.env.DATABASE_URL || "postgres://pawpad_user@localhost:5432/pawpad",
 	JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://www.usepawpad.com",
+	CLIENT_ORIGIN: "https://www.usepawpad.com",
 	// CLIENT_ORIGIN: "http://localhost:3000",
 	CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
 	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
